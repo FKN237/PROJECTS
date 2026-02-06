33a7b7beff1c97ba85047b0bc5c78d55976c2262
FKN237 <fodjonelson22@gmail.com>
2026-02-06 23:24:39 +0400

    finalised the project.

--- JAVASCRIPT/Weather App/script.js
+++ JAVASCRIPT/Weather App/script.js
@@ -4,21 +4,21 @@ const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
+
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-
-    if(response.status == 404){
-        document.querySelector(".error").style.display = "block";
-        document.querySelector(".weather").style.display = "none";
-    }
-    else{
     var data = await response.json();
-
+    if(data.cod == "404"){
+        document.querySelector(".weather").style.display = "none";
+        document.querySelector(".error").style.display = "flex";
+     }
+    else{
+    document.querySelector(".error").style.display = "none";
+    document.querySelector(".weather").style.display = "block";
     console.log(data);
     document.querySelector(".city").innerHTML = data.name
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity") + "%";
-    innerHTML = data.main.humidity;
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
     if(data.weather[0].main == "Clouds"){
@@ -35,16 +35,21 @@ async function checkWeather(city){
     }
     else if(data.weather[0].main == "Rain"){
         weatherIcon.src = "images/rain.png"
-    }
+    };
 
-   document.querySelector(".weather").style.display = "block";
-}
+    document.querySelector(".weather").style.display = "block";
+    }
 }
 
 
 searchBtn.addEventListener("click", ()=>{
+    searchBtn.classList.add("grow");
+    setTimeout(() => {
+        searchBtn.classList.remove("grow");
+    }, 200);
     checkWeather(searchBox.value);
 });
 
+searchBox.addEventListener("keydown", (e) => {if(e.key == "Enter"){checkWeather(searchBox.value);}});
 
 
