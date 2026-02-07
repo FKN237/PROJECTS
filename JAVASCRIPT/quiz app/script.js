e3bbe036a5df1136fe448f55f10f2be0efac4200
FKN237 <fodjonelson22@gmail.com>
2026-02-07 10:13:29 +0400

    Introduced new files to expand functionality
    Changes include: Modified "
    
    Added 10 new lines

---
+++ JAVASCRIPT/quiz app/script.js
@@ -0,0 +1,47 @@
+const questions =[
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+                {text: "Shark", correct: false},
+                {text: "Blue Whale", correct: true},
+                {text: "Elephant", correct: false},
+                {text: "Giraffe", correct: false}     
+        ]
+    },
+    {
+        question: "which is the smallest country in the world?",
+        answers: [
+                {text: "Vatican city", correct: true},
+                {text: "Bhutan", correct: false},
+                {text: "Nepa;", correct: false},
+                {text: "Shri Lanka", correct: false}     
+        ]
+    },
+    {
+        question: "Which is the largest desert in the world?",
+        answers: [
+                {text: "Kalahari", correct: false},
+                {text: "Gobi", correct: false},
+                {text: "Sahara", correct: false},
+                {text: "Antartica", correct: true}     
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answers: [
+                {text: "Asia", correct: false},
+                {text: "Australia", correct: true},
+                {text: "Artic", correct: false},
+                {text: "Africa", correct: false}     
+        ]
+    },
+    {
+        question: "Which is the largest mountain in the world?",
+        answers: [
+                {text: "Cameroon", correct: false},
+                {text: "Kilimanjaro", correct: false},
+                {text: "Elbrus", correct: false},
+                {text: "Everest", correct: true}     
+        ]
+    }
+]
